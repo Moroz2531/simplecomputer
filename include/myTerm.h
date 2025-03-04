22230588e02dc9d00c71fd5fd512091a60ce7373
Denis-Nosov <dnosov537@gmail.com>
2025-03-04 20:58:26 +0700

    Add new colors, #3

--- include/myTerm.h
+++ include/myTerm.h
@@ -3,11 +3,21 @@
 
 enum colors
 {
-  WHITE = 47,
-  BLACK = 16,
-  RED = 31,
-  GREEN = 40,
-  YELLOW = 11,
+  DEFAULT = 0,
+
+  FG_BLACK = 40,
+  FG_RED = 41,
+  FG_GREEN = 42,
+  FG_YELLOW = 43,
+  FG_BLUE = 44,
+  FG_WHITE = 47,
+
+  BG_WHITE = 1,
+  BG_BLACK = 30,
+  BG_RED = 31,
+  BG_GREEN = 32,
+  BG_YELLOW = 33,
+  BG_BLUE = 34,
 };
 
 int mt_clrscr ();
