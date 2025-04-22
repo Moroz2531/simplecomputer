5081c33196bf08c9989be475cb67120e84d7603b
Denis-Nosov <dnosov537@gmail.com>
2025-04-22 19:44:02 +0700

    Add input-output device, #6

---
+++ mySimpleComputer/IO-controller.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+#include "IO-controller.h"
+#include "console/print.h"
+
+#include "mySimpleComputer.h"
+#include "myTerm.h"
+
+int
+IO (int command, int operand)
+{
+  if (operand == 0)
+    return 0;
+
+  switch (command)
+    {
+    case CPUINFO:
+      mt_gotoXY (0, 26);
+      write (STDOUT_FILENO, "Носов Денис Алексеевич, ИВ-323", 52);
+      clock_pulse_generator (5);
+      mt_gotoXY (0, 26);
+      mt_delline ();
+      break;
+    case READ:
+      break;
+    case WRITE:
+      printTerm (operand, 0);
+      break;
+    default:
+      return -1;
+    }
+  return 0;
+}
---
+++ mySimpleComputer/IO-controller.h
@@ -0,0 +1,6 @@
+#ifndef IO_CONTROLLER_H
+#define IO_CONTROLLER_H
+
+int IO (int command, int operand);
+
+#endif
