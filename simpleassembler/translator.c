77f8ba2f8c311e1708706f64c2c9abc3ba7da25f
Denis-Nosov <dnosov537@gmail.com>
2025-04-30 09:05:40 +0700

    Added a part ofthe function translator, #7

--- simpleassembler/translator.c
+++ simpleassembler/translator.c
@@ -2,6 +2,32 @@
 
 #include "translator.h"
 
+#define MAX_LENGTH_COMMAND 8 // in byte
+#define MAX_LENGTH_OPERAND 4
+#define SIZE_MEMORY 128
+
+int memory[SIZE_MEMORY];
+
+enum command
+{
+  NOP = 0,
+  CPUINFO = 1,
+  READ = 10,
+  WRITE = 11,
+  LOAD = 20,
+  STORE = 21,
+  ADD = 30,
+  SUB = 31,
+  DIVIDE = 32,
+  MUL = 33,
+  JUMP = 40,
+  JNEG = 41,
+  JZ = 42,
+  HALT = 43,
+  SUBC = 66,
+  LOGLC = 67,
+};
+
 int
 translator_simple_assembler (char *filename)
 {
@@ -12,12 +38,35 @@ translator_simple_assembler (char *filename)
       return -1;
     }
 
+  short flags_str = 0, shift = 5;
+  char operand[2][MAX_LENGTH_OPERAND], command[MAX_LENGTH_COMMAND];
   char ch;
-  while ((ch = getc (file)) != EOF)
+
+  /* for statistics (output errors) */
+  int count_string = 0, pos_ch = 0;
+
+  for (char ch; (ch = getc (file)) != EOF; pos_ch++)
     {
-      printf ("%c", ch);
+      if (ch == '\n')
+        {
+          // write operands and command
+          flags_str = 0;
+          shift = 5;
+          pos_ch = 0;
+          count_string++;
+          continue;
+        }
+      if (ch == ' ' || ch == '\t' || (flags_str & 1) == 1)
+        {
+          if (flags_str != 0)
+            flags_str |= (1 << shift);
+          continue;
+        }
+      if (('0' <= ch || ch <= '9') && (flags_str == 0 || flags_str == 0x3C))
+        {
+          shift--;
+        }
     }
-  printf ("\n");
 
   fclose (file);
 
